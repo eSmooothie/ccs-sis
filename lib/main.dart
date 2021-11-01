0ffefdb06e1cf0522d697a3742f1194bf0411422
Sherwin Sandoval <33995402+eSmooothie@users.noreply.github.com>
2021-11-01 18:47:29 +0800

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,30 @@
-import 'package:ccs_sis/helper/utilities.dart';
 import 'package:ccs_sis/routes.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
+import 'package:ccs_sis/views/homepage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
+  /// initialize firebase
+  ///
+  /// You must setup your app to firebase first in order to
+  /// utilize the firebase packages
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Scaffold(
-              body: Center(
-                child: Text("Something went wrong."),
-              ),
-            );
-          }
-
-          // Once complete, show your application
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const StartApp();
-          }
-
-          // Otherwise, show something whilst waiting for initialization to complete
-          return Center(
-            child: Column(
-              children: Util.loadingCircular(),
-            ),
-          );
-        });
-  }
-}
-
-class StartApp extends StatelessWidget {
-  // This widget is the root of your application.
-  const StartApp({Key? key}) : super(key: key);
-  static FirebaseAnalytics analytics = FirebaseAnalytics();
-  static FirebaseAnalyticsObserver observer =
-      FirebaseAnalyticsObserver(analytics: analytics);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'CCS SIS',
-      onGenerateRoute: appRoutes,
-      initialRoute: homepage,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meet Up',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
     );
   }
 }
